dea302f4688b6597b16ee836289007db3446ed41
unknown <jbomfa@sga.pucminas.br>
2021-06-13 20:15:41 -0300

    first commit

---
+++ scripts/pagina_pesquisa.js
@@ -0,0 +1,7 @@
+const API_KEY = 'api_key=4aec08d6a063f531b3aa5dfd47796e60';
+const BASE_URL = 'https://api.themoviedb.org/3'
+const URL_popular = BASE_URL + '/discover/movie?sort_by=popularity.desc' +API_KEY;
+
+
+
+
